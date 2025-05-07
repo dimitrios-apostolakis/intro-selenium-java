b9041365a3e5da6d93e9c14ab90c58b7c4b109d3
dimitrios-apostolakis <dsapostolakis@gmail.com>
2025-05-07 12:49:12 +0300

    Set Chrome options for custom window positioning and implicit wait timeout for element loading reliability

--- src/Locators.java
+++ src/Locators.java
@@ -1,17 +1,35 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.awt.*;
+import java.time.Duration;
 
 public class Locators {
 
     public static void main(String[] args) {
+        // Get the location of your currently active window (IntelliJ)
+        Point location = MouseInfo.getPointerInfo().getLocation();
+        // Set up Chrome options with position
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--window-position=" + location.x + "," + (location.y-=400));
+        // Optionally set window size
+//        options.addArguments("--window-size=1200,800");
 
-        WebDriver driver = new ChromeDriver();
+
+        WebDriver driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+
         driver.get("https://rahulshettyacademy.com/locatorspractice/");
         driver.findElement(By.id("inputUsername")).sendKeys("dimitri");
         driver.findElement(By.name("inputPassword")).sendKeys("abc123");
         driver.findElement(By.className("signInBtn")).click();
         driver.findElement(By.cssSelector(".signInBtn")).click();
+        System.out.println(driver.findElement(By.className("error")).isDisplayed());
+        System.out.println(driver.findElement(By.cssSelector("p.error")).getText());
+
+
         driver.quit();
     }
 }
