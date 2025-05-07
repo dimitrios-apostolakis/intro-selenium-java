3f06e657780291bbc3f354c6101bb91d0c73be74
dimitrios-apostolakis <dsapostolakis@gmail.com>
2025-05-07 08:42:36 +0300

    Add Locators.java with Selenium login automation example

---
+++ src/Locators.java
@@ -0,0 +1,16 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Locators {
+
+    public static void main(String[] args) {
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://rahulshettyacademy.com/locatorspractice/");
+        driver.findElement(By.id("inputUsername")).sendKeys("dimitri");
+        driver.findElement(By.name("inputPassword")).sendKeys("abc123");
+        driver.findElement(By.className("signInBtn")).click();
+        driver.findElement(By.cssSelector(".signInBtn")).click();
+    }
+}
