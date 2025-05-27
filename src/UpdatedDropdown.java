969960c673a8fe6fceb75cfaca28da5ea13908c4
dimitrios-apostolakis <dsapostolakis@gmail.com>
2025-05-27 11:52:46 +0300

    Add dropdown handling example with Selenium

---
+++ src/UpdatedDropdown.java
@@ -0,0 +1,29 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.awt.*;
+
+public class UpdatedDropdown {
+
+    public static void main(String[] args) throws InterruptedException {
+        Point location = MouseInfo.getPointerInfo().getLocation();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--window-position=" + location.x + "," + (location.y -= 400));
+
+        WebDriver driver = new ChromeDriver(options);
+        driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
+        System.out.println(driver.findElement(By.id("divpaxinfo")).getText());
+        driver.findElement(By.id("divpaxinfo")).click();
+        Thread.sleep(1000L);
+        int i=0;
+        while (i<2){
+            driver.findElement(By.id("hrefIncAdt")).click();
+            i++;
+        }
+        driver.findElement(By.id("btnclosepaxoption")).click();
+        System.out.println(driver.findElement(By.id("divpaxinfo")).getText());
+        driver.quit();
+    }
+}
