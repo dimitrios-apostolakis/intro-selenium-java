dc32a8b2686f0ce608aca4d1b9cf122daab6cb5f
dimitrios-apostolakis <dsapostolakis@gmail.com>
2025-05-28 16:23:50 +0300

    Add base script for Selenium WebDriver automation

---
+++ src/Base.java
@@ -0,0 +1,35 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.awt.*;
+import java.util.Arrays;
+import java.util.List;
+
+public class Base {
+
+    public static void main(String[] args) {
+        Point location = MouseInfo.getPointerInfo().getLocation();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--window-position=" + location.x + "," + (location.y -= 400));
+
+        WebDriver driver = new ChromeDriver(options);
+        driver.get("https://rahulshettyacademy.com/seleniumPractise/#/");
+        String[] items = {"Cucumber", "Brocolli"};
+
+        List<WebElement> products = driver.findElements(By.className("product-name"));
+        int count = 0;
+        for (int i=0; i< products.size(); i++){
+            String name = products.get(i).getText();
+            name = name.split("-")[0].trim();
+            if (Arrays.asList(items).contains(name)){
+                driver.findElements(By.xpath("//div[@class='product-action']/button")).get(i).click();
+//                driver.findElements(By.xpath("//button[text()='ADD TO CART' or text()='✔ ADDED']")).get(i).click();
+                count++;
+                if (count == items.length) break;
+            }
+        }
+    }
+}
